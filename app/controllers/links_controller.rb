94f2889fe496a940e7a3503068447aa4ffed9f40
Brian Rippeto <rippeto@gmail.com>
2016-06-06 14:11:54 -0600

    User can post links and only view links associated with there account

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,12 +2,13 @@ class LinksController < ApplicationController
 
   def index
     @new_link = Link.new
-    @links = Link.all
+    @links = Link.all.where(user_id: current_user.id)
   end
 
   def create
     @link = Link.new(link_params)
     if @link.save
+      @link.update_attribute(:user_id, current_user.id)
       flash[:success] = "You have saved a link!"
       redirect_to links_path
     else
@@ -15,7 +16,7 @@ class LinksController < ApplicationController
       render :index
     end
   end
-  
+
 private
   def link_params
     params.require(:link).permit(:title, :url)
