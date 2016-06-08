6d3c328d41c4ce2077bdf12ed8abcae3b6df8e0a
Brian Rippeto <rippeto@gmail.com>
2016-06-07 19:16:03 -0600

    remove comments

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,18 +1,6 @@
 class Api::V1::LinksController < ApplicationController
   respond_to :json
 
-  # def index
-  #   respond_with Idea.all
-  # end
-  #
-  # def create
-  #   respond_with Idea.create(idea_params), location: nil
-  # end
-  #
-  # def destroy
-  #   respond_with Idea.destroy(params[:id]), location: nil
-  # end
-
   def update
     respond_with Link.update(params[:id], link_params), location: nil
   end
